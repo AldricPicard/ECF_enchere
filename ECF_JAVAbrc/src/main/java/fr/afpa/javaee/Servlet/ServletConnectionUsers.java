7b6118cd7ca6175b8626a0c30f76b58c2458068d
unknown <thomasbaracani@gmail.com>
2023-04-06 14:49:10 +0200

    cookie
    cookie pour le btn "se souvenir de moi"

--- ECF_JAVAbrc/src/main/java/fr/afpa/javaee/Servlet/ServletConnectionUsers.java
+++ ECF_JAVAbrc/src/main/java/fr/afpa/javaee/Servlet/ServletConnectionUsers.java
@@ -1,14 +1,17 @@
 package fr.afpa.javaee.Servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import fr.afpa.javaee.bll.*;
+import fr.afpa.javaee.bll.BusinessException;
+import fr.afpa.javaee.bll.UsersManager;
 import fr.afpa.javaee.bo.Users;
 
 
@@ -23,6 +26,14 @@ public class ServletConnectionUsers extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Cookie[] cookies = request.getCookies();
+		   for (Cookie cookie : cookies) {
+	            if (cookie.getName().equals(request.getParameter("username"))) {
+	                System.out.println(cookie.getValue());
+	                request.setAttribute("pseudo", cookie.getValue());
+	            }
+	        }
+		
 		request.setCharacterEncoding("UTF-8");
 		request.getRequestDispatcher("/WEB-INF/connectionJSP.jsp").forward(request, response);
 	}
@@ -36,6 +47,14 @@ public class ServletConnectionUsers extends HttpServlet {
 		String password = request.getParameter("password");
 		request.setAttribute("username", username);
 		request.setAttribute("password", password);
+		
+		
+		if (request.getParameter("cookie_remember_me") != null) { // Si la case "se souvenir de moi" est coché créer le cookie
+            Cookie monCookie = new Cookie("username", username);
+            monCookie.setMaxAge(60 * 60 * 24 * 30); // Durée de vie du cookie 1mois 
+            response.addCookie(monCookie);
+        }
+		
 		
 		try {
 			Users user = userManager.envoiDonnePourConnection(username,password);
